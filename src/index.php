6da999b18bc0062e8cbc4319add916a812a2ffab
pragnesh-telstra <45471673+pragnesh-telstra@users.noreply.github.com>
2018-11-30 11:01:29 +1100

    another change to test approval steps

--- src/index.php
+++ src/index.php
@@ -16,7 +16,7 @@
         <div class="container">
             <div class="hero-unit">
                 <h1>Pragnesh Telstra CI/CD Pipeline</h1>
-                <h2>Congratulations</h2>
+                <h2>Congratulations! You have added approval now</h2>
                 <p>Your AWS application is now running on a container in Amazon ECS.</p>
                 <p>The container is running PHP version <?php echo phpversion(); ?>.</p>
                 <?php
